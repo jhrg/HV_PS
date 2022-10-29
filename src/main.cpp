c54e60cb5c8dec283c02b0ca51b5b87fb7488f9b
James Gallagher <jgallagher@opendap.org>
2022-10-29 10:10:19 -0600

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -4,60 +4,33 @@
 */
 #include <Arduino.h>
 
-#define LOGGING 1
+#define PID_LOGGING 0
+#define SIMPLE_LOGGING 0
+
+#define ADC_NOISE 3
+#define SAMPLE_PERIOD 500 // ms
+#define SET_POINT 455     // ~ 200v
+
+#if 0
+// these work for no load, but don't respond quickly with a load
+#define Kp 0.009
+#define Ki 0.0000009
+#define Kd 10
+#endif
+
+#if 1
+// works with 14.8 mA, and 1 mA, sort of...
+#define Kp 0.04
+#define Ki 0.00005
+#define Kd 10
+#endif
 
 int32_t last_time = 0;
 // uint32_t last_voltage = 0;
 int32_t last_error = 0;
 int32_t cum_error = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-
-  Serial.begin(9600);
-  Serial.println("boot");
-
-  // Set all ports to be outputs
-  // Initialize all I/O pins to output, then one for the analog input
-  DDRD = B11111111;
-  DDRC = B00111111;
-  DDRB = B00111111;
-
-  pinMode(A0, INPUT);
-
-  // Use Timer1 for the HV PS control signal
-  // Set the timer to Fast PWM. COM1A1:0 --> 1, 0
-  // Set the timer for 10-bit resolution. WGM13:0 --> 0, 1, 1, 1
-  // Set the timer for 9-bit resolution. WGM13:0 --> 0, 1, 1, 0
-  TCCR1A = _BV(COM1A1) | _BV(WGM11);
-  // Set the pre-scaler at 1 (62.5 kHz) and the two high-order bits of WGM
-  TCCR1B = _BV(WGM12) | _BV(CS10);
-
-  OCR1A = 0x10; // 10-bit resolution --> 0x0000 - 0x03FF
-
-#if LOGGING
-  Serial.println(" voltage, error, delta_t, cum_error, rate_error, correction, OCR1A");
-#endif
-
-  last_time = millis();
-}
-
-#define SET_POINT 455 // ~ 200v
-
-#define Kp 0.01
-#define Ki 0
-#define Kd 0
-
-#define ADC_NOISE 3
-
-#define SAMPLE_PERIOD 10 // ms
-
-void loop()
-{
-  int32_t now = millis();
-
-  if ((now - last_time >= SAMPLE_PERIOD))
-  {
+int32_t pid_correction(int32_t now) {
     // delta time
     int32_t delta_t = now - last_time;
 
@@ -77,30 +50,116 @@ void loop()
     last_time = now;
     last_error = error;
 
-#if LOGGING
+#if PID_LOGGING
     char str[128];
     snprintf(str, 64, "%ld, %ld, %ld, %ld, %ld, %ld, %d", voltage, error, delta_t, cum_error, rate_error, correction, OCR1A);
     Serial.println(str);
 #endif
 
-    OCR1A += correction;
-  }
+    return correction;
+}
+
+int32_t simple_correction(int32_t now) {
+    int32_t voltage = analogRead(A0); // 0 - 1023 -> 0 - 5v;
+
+    // Cap the OCR0A value between 0x010 and 0x1F0 to avoid extremes
+    if ((voltage > SET_POINT + ADC_NOISE) && (OCR1A > 0x0010)) {
+        return -1;
+    } else if ((voltage < SET_POINT - ADC_NOISE) && (OCR1A < 0x01F0)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int32_t two_factor_correction(int32_t now) {
+    int32_t voltage = analogRead(A0); // 0 - 1023 -> 0 - 5v;
+    int32_t error = SET_POINT - voltage;
+
+    // Cap the OCR0A value between 0x010 and 0x1F0 to avoid extremes
+    if ((voltage > SET_POINT + ADC_NOISE) && (OCR1A > 0x0010)) {
+        return (error < -20) ? -2 : -1;
+    } else if ((voltage < SET_POINT - ADC_NOISE) && (OCR1A < 0x01F0)) {
+        return (error > 20) ? 2 : 1;
+    } else {
+        return 0;
+    }
 #if 0
     // Cap the OCR0A value between 0x010 and 0x1F0 to avoid extremes
-    if (voltage > SET_POINT + ADC_NOISE)
-    {
-      if (OCR1A > 0x0010) {
-        // OCR1A -= 1;
-        OCR1A -= (correction > 1) ? correction : 1;
-      }
+    if (voltage > SET_POINT + ADC_NOISE) {
+        if (OCR1A > 0x0010) {
+            return -1;
+            // OCR1A -= 1;
+            //OCR1A -= (correction > 1) ? correction : 1;
+        }
+    } else if (voltage < SET_POINT - ADC_NOISE) {
+        if (OCR1A < 0x01F0) {
+            return 1;
+            // OCR1A += 1;
+            //OCR1A += (correction > 1) ? correction : 1;
+        }
     }
-    else if (voltage < SET_POINT - ADC_NOISE)
-    {
-      if (OCR1A < 0x01F0) {
-        // OCR1A += 1;
-        OCR1A += (correction > 1) ? correction : 1;
-      }
-    }
-  }
 #endif
+}
+
+void setup() {
+    // put your setup code here, to run once:
+
+    Serial.begin(115200);
+    Serial.println("boot");
+
+    // Set all ports to be outputs
+    // Initialize all I/O pins to output, then one for the analog input
+    DDRD = B11111111;
+    DDRC = B00111111;
+    DDRB = B00111111;
+
+    pinMode(A0, INPUT);
+
+    // Use Timer1 for the HV PS control signal
+    // Set the timer to Fast PWM. COM1A1:0 --> 1, 0
+    // Set the timer for 9-bit resolution. WGM13:0 --> 0, 1, 1, 0
+    TCCR1A = _BV(COM1A1) | _BV(WGM11);
+    // Set the pre-scaler at 1 (31.25 kHz) and WGM bit 2
+    TCCR1B = _BV(WGM12) | _BV(CS10);
+
+    // Start out with low voltage
+    OCR1A = 0x010; // 9-bit resolution --> 0x0000 - 0x01FF
+
+#if PID_LOGGING
+    Serial.println("voltage, error, delta_t, cum_error, rate_error, correction, OCR1A");
+#elif SIMPLE_LOGGING
+    Serial.println("voltage, OCR1A");
+#endif
+
+    last_time = millis();
+}
+
+void loop() {
+    int32_t now = millis();
+
+    if ((now - last_time >= SAMPLE_PERIOD)) {
+        int32_t voltage = analogRead(A0); // 0 - 1023 -> 0 - 5v;
+
+        // OCR1A += simple_correction(now);
+        // OCR1A += two_factor_correction(now);
+        // OCR1A += pid_correction(now);
+
+        // Cap the OCR0A value between 0x010 and 0x1F0 to avoid extremes
+        if (voltage > SET_POINT + ADC_NOISE) {
+            if (OCR1A > 0x0010) {
+                OCR1A -= 1;
+            }
+        } else if (voltage < SET_POINT - ADC_NOISE) {
+            if (OCR1A < 0x01F0) {
+                OCR1A += 1;
+            }
+        }
+
+#if SIMPLE_LOGGING
+        char str[128];
+        snprintf(str, 64, "%ld, %d", voltage, OCR1A);
+        Serial.println(str);
+#endif
+    }
 }
