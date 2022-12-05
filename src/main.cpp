9cd45e225b9a8189a4f6c645a99a02f397c22664
James Gallagher <jhrg@mac.com>
2022-12-04 20:54:24 -0700

    Removed all the unused code

--- src/main.cpp
+++ src/main.cpp
@@ -13,110 +13,7 @@
 double input = 80, output = 50, setpoint = SET_POINT;
 double kp = 0.8, ki = 0.4, kd = 0.0;
 
-#if 0
-//double kpmodel = 1.5, taup = 100, theta[50];
-double outputStart = 5;
-double aTuneStep = 25, aTuneNoise = 4, aTuneStartValue = 100;
-unsigned int aTuneLookBack = 20;
-
-boolean tuning = false;
-unsigned long serialTime = 0;
-#endif
-
 PID myPID(&input, &output, &setpoint, kp, ki, kd, DIRECT);
-
-#if 0
-PID_ATune aTune(&input, &output);
-int32_t last_time = 0;
-float last_error = 0;
-float cum_error = 0;
-#endif
-
-#if 0
-int32_t pid_correction(int32_t delta_t, int32_t voltage) {
-    // delta time
-    // int32_t delta_t = (now - last_time);
-
-    float error = (float)(SET_POINT - voltage);
-
-    cum_error += error * (float)delta_t;
-    float rate_error = (error - last_error) / (float)delta_t;
-
-    int32_t correction = Kp * error + Ki * cum_error + Kd * rate_error;
-
-    // last_time is updated by the caller
-    last_error = error;
-
-#if PID_LOGGING
-    char str[128];
-#define frac(f) ((int)(f * 100) % 100)
-    snprintf(str, 64, "%ld, %ld, %ld, %ld, %ld, %ld, %d",
-             voltage, (long)error, delta_t, (long)cum_error, (long)rate_error, correction, OCR2B);
-    Serial.println(str);
-#endif
-
-    return correction;
-}
-#endif
-
-#if 0
-void AutoTuneHelper(boolean start) {
-    if (start)
-        ATuneModeRemember = myPID.GetMode();
-    else
-        myPID.SetMode(ATuneModeRemember);
-}
-
-void changeAutoTune() {
-    if (!tuning) {
-        // Set the output to the desired starting frequency.
-        output = aTuneStartValue;
-        aTune.SetNoiseBand(aTuneNoise);
-        aTune.SetOutputStep(aTuneStep);
-        aTune.SetLookbackSec((int)aTuneLookBack);
-        AutoTuneHelper(true);
-        tuning = true;
-    } else { // cancel autotune
-        aTune.Cancel();
-        tuning = false;
-        AutoTuneHelper(false);
-    }
-}
-
-void SerialSend() {
-    Serial.print("setpoint: ");
-    Serial.print(setpoint);
-    Serial.print(" ");
-    Serial.print("input: ");
-    Serial.print(input);
-    Serial.print(" ");
-    Serial.print("output: ");
-    Serial.print(output);
-    Serial.print(" ");
-    if (tuning) {
-        Serial.println("tuning mode");
-    } else {
-        Serial.print("kp: ");
-        Serial.print(myPID.GetKp());
-        Serial.print(" ");
-        Serial.print("ki: ");
-        Serial.print(myPID.GetKi());
-        Serial.print(" ");
-        Serial.print("kd: ");
-        Serial.print(myPID.GetKd());
-        Serial.println();
-    }
-}
-
-void SerialReceive() {
-    if (Serial.available()) {
-        char b = Serial.read();
-        Serial.flush();
-        if ((b == '1' && !tuning) || (b != '1' && tuning))
-            changeAutoTune();
-    }
-}
-#endif
 
 void setup() {
     // put your setup code here, to run once:
@@ -155,54 +52,10 @@ void setup() {
     myPID.SetOutputLimits(10, 150);
     myPID.SetSampleTime(SAMPLE_PERIOD);
     myPID.SetMode(AUTOMATIC);
-
-#if 0
-    if (tuning) {
-        tuning = false;
-        changeAutoTune();
-        tuning = true;
-    }
-
-    last_time = millis();
-#endif
 }
 
 void loop() {
-
     input = analogRead(A0);
-
-#if 0
-    if (tuning) {
-        byte val = (aTune.Runtime());
-        if (val != 0) {
-            tuning = false;
-        }
-        if (!tuning) { // we're done, set the tuning parameters
-            kp = aTune.GetKp();
-            ki = aTune.GetKi();
-            kd = aTune.GetKd();
-            myPID.SetTunings(kp, ki, kd);
-            AutoTuneHelper(false);
-        }
-    } else {
-        myPID.Compute();
-    }
-#endif
-
     myPID.Compute();
     analogWrite(3, output);
-
-#if 0
-    // send-receive with processing if it's time
-    if (millis() > serialTime) {
-        SerialReceive();
-        SerialSend();
-        serialTime += 500;
-    }
-#endif
-#if 0
-    char str[128];
-    snprintf(str, 64, "%ld, %d", (long)input, OCR2B);
-    Serial.println(str);
-#endif
 }
