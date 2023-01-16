95d44faf03747df1f41be21f1ebfe1556562bf77
James Gallagher <jgallagher@opendap.org>
2023-01-16 09:22:12 -0700

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
  */
 #include <Arduino.h>
 
-//#define ADC_NOISE 3      // counts
 #define HV_PS_INPUT A0
 #define SAMPLE_PERIOD 10 // ms
 #define SET_POINT 455    // ~ 200v
@@ -32,9 +31,9 @@ void setup() {
 
     cli();
 
-    TCCR1A = 0;  // set entire TCCR2A register to 0
-    TCCR1B = 0;  // same for TCCR0B
-    TCNT1 = 0;   // initialize counter value to 0
+    TCCR1A = 0; // set entire TCCR2A register to 0
+    TCCR1B = 0; // same for TCCR0B
+    TCNT1 = 0;  // initialize counter value to 0
 
     // Use Timer1 for the HV PS control signal
     // Set the timer to Fast PWM. COM1A1:0 --> 1, 0
@@ -44,17 +43,17 @@ void setup() {
     // Set the pre-scaler at 1 (62.5 kHz) and the two high-order bits of WGM
     TCCR1B = _BV(WGM12) | _BV(CS10);
 
-    OCR1B = 0xFF;  // 9-bit resolution --> 0x0000 - 0x01FF
+    OCR1B = 0xAF; // 9-bit resolution --> 0x0000 - 0x01FF
 
     sei();
 
     input = analogRead(HV_PS_INPUT);
     myPID.SetOutputLimits(10, 150);
     myPID.SetSampleTime(SAMPLE_PERIOD);
-    myPID.SetMode(AUTOMATIC);  // This turns on the PID; MANUAL mode turns it off
+    myPID.SetMode(AUTOMATIC); // This turns on the PID; MANUAL mode turns it off
 }
 
-/** 
+/**
  * @brief The simplest input reader. Always reads a value
  */
 bool read_input(double *in) {
@@ -72,5 +71,4 @@ void loop() {
     PORTD &= ~_BV(PORTD6);
 #endif
     OCR1B = (unsigned char)output;
-    // analogWrite(3, output);
 }
